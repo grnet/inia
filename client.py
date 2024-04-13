6f9b22458ab947f13206c5e607f1330e2d115a03
Spyros Ligouras <ligouras@admin.grnet.gr>
2024-04-13 05:44:38 +0300

    Implement scrolling

---
+++ client.py
@@ -0,0 +1,61 @@
+import requests
+
+from boto3.session import Session
+from requests_aws4auth import AWS4Auth
+
+
+class AWSBaseClientMixin:
+    AMZ_USER_AGENT = "aws-sdk-js/2.1467.0 promise"
+
+    def __init__(
+        self,
+        access_key,
+        secret_key,
+        token=None,
+        region=None,
+        service=None,
+        endpoint=None,
+    ):
+        self.headers = {
+            "accept": "*/*",
+            "content-type": "application/x-amz-json-1.1",
+            "x-amz-user-agent": self.AMZ_USER_AGENT,
+        }
+
+        self.session = Session(access_key, secret_key, token)
+
+        self.region = region
+        self.service = service
+        self.endpoint = endpoint
+
+    def _auth(self):
+        assert self.region is not None, "Region must be set before authenticating"
+        assert self.service is not None, "Service must be set before authenticating"
+
+        credentials = self.session.get_credentials().get_frozen_credentials()
+
+        self.auth = AWS4Auth(
+            credentials.access_key,
+            credentials.secret_key,
+            self.region,
+            self.service,
+            session_token=credentials.token,
+        )
+
+    def _make_request(self, target, data):
+        assert self.endpoint is not None, "Endpoint must be set before making a request"
+
+        if self.auth is None:
+            self._auth()
+
+        self.headers.update(
+            {
+                "x-amz-target": target,
+            }
+        )
+        response = requests.post(
+            self.endpoint, auth=self.auth, headers=self.headers, json=data
+        )
+        response.raise_for_status()
+
+        return response.json()
---
+++ singlesignon/client.py
@@ -0,0 +1,129 @@
+from inia.client import AWSBaseClientMixin
+
+
+class SingleSignOnClient(AWSBaseClientMixin):
+    def __init__(self, access_key, secret_key, region, token=None):
+        super().__init__(access_key, secret_key, token=token, region=region)
+
+        self.service = "userpool"
+        self.endpoint = f"https://up.sso.{region}.amazonaws.com/"
+
+        self.sso_admin = self.session.client("sso-admin")
+        self.identitystore = self.session.client("identitystore")
+
+    def list_instances(self):
+        instances = []
+
+        if self.sso_admin.can_paginate("list_instances"):
+            paginator = self.sso_admin.get_paginator("list_instances")
+            for page in paginator.paginate():
+                instances.extend(page["Instances"])
+        else:
+            instances = self.sso_admin.list_instances()["Instances"]
+
+        return instances
+
+    def list_permission_sets(self, instance_arn):
+        permission_set_arns = []
+
+        if self.sso_admin.can_paginate("list_permission_sets"):
+            paginator = self.sso_admin.get_paginator("list_permission_sets")
+            for page in paginator.paginate(InstanceArn=instance_arn):
+                permission_set_arns.extend(page["PermissionSets"])
+        else:
+            permission_set_arns = self.sso_admin.list_permission_sets(
+                InstanceArn=instance_arn
+            )["PermissionSets"]
+
+        permission_sets = []
+        for ps_arn in permission_set_arns:
+            ps = self.sso_admin.describe_permission_set(
+                InstanceArn=instance_arn, PermissionSetArn=ps_arn
+            )
+            permission_sets.append(ps["PermissionSet"])
+
+        return permission_sets
+
+    def list_accounts_for_provisioned_permission_set(
+        self, instance_arn, permission_set_arn
+    ):
+        accounts = []
+
+        if self.sso_admin.can_paginate("list_accounts_for_provisioned_permission_set"):
+            paginator = self.sso_admin.get_paginator(
+                "list_accounts_for_provisioned_permission_set"
+            )
+            for page in paginator.paginate(
+                InstanceArn=instance_arn, PermissionSetArn=permission_set_arn
+            ):
+                accounts.extend(page["AccountIds"])
+        else:
+            accounts = self.sso_admin.list_accounts_for_provisioned_permission_set(
+                InstanceArn=instance_arn, PermissionSetArn=permission_set_arn
+            )["AccountIds"]
+
+        return accounts
+
+    def list_account_assignments(self, instance_arn, permission_set_arn, account_id):
+        assignments = []
+
+        if self.sso_admin.can_paginate("list_account_assignments"):
+            paginator = self.sso_admin.get_paginator("list_account_assignments")
+            for page in paginator.paginate(
+                InstanceArn=instance_arn,
+                PermissionSetArn=permission_set_arn,
+                AccountId=account_id,
+            ):
+                assignments.extend(page["AccountAssignments"])
+        else:
+            assignments = self.sso_admin.list_account_assignments(
+                InstanceArn=instance_arn,
+                PermissionSetArn=permission_set_arn,
+                AccountId=account_id,
+            )["AccountAssignments"]
+
+        return assignments
+
+    def list_users(self, identity_store_id):
+        users = []
+
+        if self.identitystore.can_paginate("list_users"):
+            paginator = self.identitystore.get_paginator("list_users")
+            for page in paginator.paginate(IdentityStoreId=identity_store_id):
+                users.extend(page["Users"])
+        else:
+            users = self.identitystore.list_users(IdentityStoreId=identity_store_id)[
+                "Users"
+            ]
+
+        return users
+
+    def list_groups(self, identity_store_id):
+        groups = []
+
+        if self.identitystore.can_paginate("list_groups"):
+            paginator = self.identitystore.get_paginator("list_groups")
+            for page in paginator.paginate(IdentityStoreId=identity_store_id):
+                groups.extend(page["Groups"])
+        else:
+            groups = self.identitystore.list_groups(IdentityStoreId=identity_store_id)[
+                "Groups"
+            ]
+
+        return groups
+
+    def list_group_memberships(self, identity_store_id, group_id):
+        memberships = []
+
+        if self.identitystore.can_paginate("list_group_memberships"):
+            paginator = self.identitystore.get_paginator("list_group_memberships")
+            for page in paginator.paginate(
+                IdentityStoreId=identity_store_id, GroupId=group_id
+            ):
+                memberships.extend(page["GroupMemberships"])
+        else:
+            memberships = self.identitystore.list_group_memberships(
+                IdentityStoreId=identity_store_id, GroupId=group_id
+            )["GroupMemberships"]
+
+        return memberships
