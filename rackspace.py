13c3ecacfd96eed6ff4c9a75d63a3e2dc83b158d
Michael Loukeris <milouk@admin.grnet.gr>
2024-04-24 17:07:59 +0300

    Add rackspace client

---
+++ rackspace.py
@@ -0,0 +1,72 @@
+import json
+import os
+from typing import Any, Dict
+
+import requests
+
+
+class RackspaceClient:
+    RACKSPACE_TOKEN_URL = "https://identity.api.rackspacecloud.com/v2.0/tokens"
+    AWS_ACCOUNTS_URL = "https://accounts.api.manage.rackspace.com/v0/awsAccounts"
+    CREDENTIALS_URL_TEMPLATE = (
+        "https://accounts.api.manage.rackspace.com/v0/awsAccounts/{}/credentials"
+    )
+    DEFAULT_REGION = "eu-central-1"
+
+    def __init__(self, username: str, rackspace_api_key: str):
+        self.username = username
+        self.rackspace_api_key = rackspace_api_key
+
+    def get_rackspace_token(self) -> Dict[str, Any]:
+        headers = {"Content-Type": "application/json"}
+        data = json.dumps(
+            {
+                "auth": {
+                    "RAX-KSKEY:apiKeyCredentials": {
+                        "username": self.username,
+                        "apiKey": self.rackspace_api_key,
+                    }
+                }
+            }
+        )
+        response = requests.post(self.RACKSPACE_TOKEN_URL, headers=headers, data=data)
+        response.raise_for_status()
+        return response.json()["access"]["token"]
+
+    def get_aws_accounts(self, token_id: str, tenant_id: str) -> Dict[str, Any]:
+        headers = {
+            "X-Auth-Token": token_id,
+            "X-Tenant-Id": tenant_id,
+            "Content-Type": "application/json",
+        }
+        response = requests.get(self.AWS_ACCOUNTS_URL, headers=headers)
+        response.raise_for_status()
+        return response.json()
+
+    def get_credentials(
+        self, token_id: str, tenant_id: str, aws_account_number: str
+    ) -> Dict[str, Any]:
+        url = self.CREDENTIALS_URL_TEMPLATE.format(aws_account_number)
+        data = '{"credential": {"duration": "3600"}}'
+        headers = {
+            "X-Auth-Token": token_id,
+            "X-Tenant-Id": tenant_id,
+            "Content-Type": "application/json",
+        }
+        response = requests.post(url, headers=headers, data=data)
+        response.raise_for_status()
+        return response.json()
+
+    def setup_aws_credentials(self, credentials: Dict[str, str]) -> Dict[str, str]:
+        env_keys = {
+            "accessKeyId": "AWS_ACCESS_KEY_ID",
+            "secretAccessKey": "AWS_SECRET_ACCESS_KEY",
+            "sessionToken": "AWS_SESSION_TOKEN",
+        }
+
+        for key, env_key in env_keys.items():
+            os.environ[env_key] = credentials[key]
+        os.environ["AWS_DEFAULT_REGION"] = self.DEFAULT_REGION
+        return {env_key: credentials[key] for key, env_key in env_keys.items()} | {
+            "region": self.DEFAULT_REGION
+        }
