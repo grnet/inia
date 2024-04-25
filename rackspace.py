c44c31ad1b87dd69283b83a158078d2cb76c0fc6
Spyros Ligouras <ligouras@admin.grnet.gr>
2024-04-25 06:11:48 +0300

    Simplify rackspace client

--- rackspace.py
+++ rackspace.py
@@ -1,6 +1,3 @@
-import json
-from typing import Any, Dict
-
 import requests
 
 
@@ -10,48 +7,41 @@ class RackspaceClient:
     CREDENTIALS_URL_TEMPLATE = (
         "https://accounts.api.manage.rackspace.com/v0/awsAccounts/{}/credentials"
     )
-    DEFAULT_REGION = "eu-central-1"
 
-    def __init__(self, username: str, rackspace_api_key: str):
+    def __init__(self, username, rackspace_api_key):
         self.username = username
         self.rackspace_api_key = rackspace_api_key
 
-    def get_rackspace_token(self) -> Dict[str, Any]:
-        headers = {"Content-Type": "application/json"}
-        data = json.dumps(
-            {
-                "auth": {
-                    "RAX-KSKEY:apiKeyCredentials": {
-                        "username": self.username,
-                        "apiKey": self.rackspace_api_key,
-                    }
+    def get_rackspace_token(self):
+        data = {
+            "auth": {
+                "RAX-KSKEY:apiKeyCredentials": {
+                    "username": self.username,
+                    "apiKey": self.rackspace_api_key,
                 }
             }
-        )
-        response = requests.post(self.RACKSPACE_TOKEN_URL, headers=headers, data=data)
+        }
+
+        response = requests.post(self.RACKSPACE_TOKEN_URL, json=data)
         response.raise_for_status()
         return response.json()["access"]["token"]
 
-    def get_aws_accounts(self, token_id: str, tenant_id: str) -> Dict[str, Any]:
+    def get_aws_accounts(self, token_id, tenant_id):
         headers = {
             "X-Auth-Token": token_id,
             "X-Tenant-Id": tenant_id,
-            "Content-Type": "application/json",
         }
         response = requests.get(self.AWS_ACCOUNTS_URL, headers=headers)
         response.raise_for_status()
         return response.json()
 
-    def get_credentials(
-        self, token_id: str, tenant_id: str, aws_account_number: str
-    ) -> Dict[str, Any]:
+    def get_credentials(self, token_id, tenant_id, aws_account_number):
         url = self.CREDENTIALS_URL_TEMPLATE.format(aws_account_number)
-        data = '{"credential": {"duration": "3600"}}'
+        data = {"credential": {"duration": "3600"}}
         headers = {
             "X-Auth-Token": token_id,
             "X-Tenant-Id": tenant_id,
-            "Content-Type": "application/json",
         }
-        response = requests.post(url, headers=headers, data=data)
+        response = requests.post(url, headers=headers, json=data)
         response.raise_for_status()
         return response.json()
