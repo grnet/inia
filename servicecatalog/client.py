7686fc748eb33afdbe9b080354ab5e00b6aaff3f
Michael Loukeris <milouk@admin.grnet.gr>
2024-05-10 14:01:07 +0300

    Refactor servicecatalog client

--- servicecatalog/client.py
+++ servicecatalog/client.py
@@ -1,51 +1,40 @@
-import time
-
 from inia.client import AWSBaseClientMixin
 
 
 class ServiceCatalogClient(AWSBaseClientMixin):
     def __init__(self, access_key, secret_key, token=None, region="eu-central-1"):
         super().__init__(access_key, secret_key, token=token, region=region)
+        self.servicecatalog = self.session.client("servicecatalog")
 
-        self.service = "servicecatalog"
-        self.servicecatalog = self.session.client(self.service)
-
-    def search_provisioned_products(self):
-        response = self.servicecatalog.search_provisioned_products(
+    def _search_provisioned_products(self):
+        return self.servicecatalog.search_provisioned_products(
             AccessLevelFilter={"Key": "Account", "Value": "self"}
         )
-        return response
 
     def get_provisioned_product_id(self, product_name):
-        pp = self.search_provisioned_products()
-        for product in pp["ProvisionedProducts"]:
-            if product["Name"] == product_name:
-                return product["Id"]
-        return None
+        pp = self._search_provisioned_products()
+        return next(
+            (
+                product["Id"]
+                for product in pp["ProvisionedProducts"]
+                if product["Name"] == product_name
+            ),
+            None,
+        )
 
-    def check_pp(self, pp_id):
-        while True:
-            response = self.servicecatalog.describe_provisioned_product(Id=pp_id)
-            state = response["ProvisionedProductDetail"]["Status"]
+    def get_provisioned_product_detail(self, pp_id):
+        response = self.servicecatalog.describe_provisioned_product(Id=pp_id)
+        return response["ProvisionedProductDetail"]["Status"]
 
-            if state == "AVAILABLE":
-                print(f"Status: {state}, provisioning succeeded")
-                break
-            elif state == "TAINTED":
-                print(f"Status: {state}, provisioning tainted")
-                exit(1)
-            elif state == "ERROR":
-                print(f"Status: {state}, provisioning failed")
-                exit(1)
-
-            print(f"Status: {state}, sleeping for 30s ...")
-            time.sleep(30)
-
+    def get_provisioned_product_outputs(self, pp_id):
         outputs = self.servicecatalog.get_provisioned_product_outputs(
             ProvisionedProductId=pp_id
         )
-        for output in outputs["Outputs"]:
-            if output["OutputKey"] == "AccountId":
-                return output["OutputValue"]
-
-        return None
+        return next(
+            (
+                output["OutputValue"]
+                for output in outputs["Outputs"]
+                if output["OutputKey"] == "AccountId"
+            ),
+            None,
+        )
