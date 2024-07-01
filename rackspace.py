24b247a96b93b7cf782f6c4945d661ec13e1c977
Michael Loukeris <milouk@admin.grnet.gr>
2024-07-01 15:56:48 +0300

    Add function to provision account to default and to get workflow details

--- rackspace.py
+++ rackspace.py
@@ -7,6 +7,8 @@ class RackspaceClient:
     CREDENTIALS_URL_TEMPLATE = (
         "https://accounts.api.manage.rackspace.com/v0/awsAccounts/{}/credentials"
     )
+    PROVISION_ACCOUNT_DEFAULTS_URL = "https://manage.rackspace.com/aws/api/accounts/v0/awsAccounts/{}/provisioningWorkflows"
+    GET_PROVISION_DETAILS_URL = "https://manage.rackspace.com/aws/api/accounts/v0/awsAccounts/{}/provisioningDetails"
 
     def __init__(self, username, rackspace_api_key):
         self.username = username
@@ -43,5 +45,28 @@ class RackspaceClient:
             "X-Tenant-Id": tenant_id,
         }
         response = requests.post(url, headers=headers, json=data)
+        response.raise_for_status()
+        return response.json()
+
+    def rs_aws_provision_account_defaults(
+        self, token_id, tenant_id, aws_account_number
+    ):
+        url = self.PROVISION_ACCOUNT_DEFAULTS_URL.format(aws_account_number)
+        data = {"dryRun": False}
+        headers = {
+            "X-Auth-Token": token_id,
+            "X-Tenant-Id": tenant_id,
+        }
+        response = requests.post(url, headers=headers, json=data)
+        response.raise_for_status()
+        return response.json()
+
+    def rs_aws_get_provision_details(self, token_id, tenant_id, aws_account_number):
+        url = self.GET_PROVISION_DETAILS_URL.format(aws_account_number)
+        headers = {
+            "X-Auth-Token": token_id,
+            "X-Tenant-Id": tenant_id,
+        }
+        response = requests.get(url, headers=headers)
         response.raise_for_status()
         return response.json()
