538b94582dca943f18cf18cb2b5a397923c6d64a
Michael Loukeris <milouk@admin.grnet.gr>
2024-09-12 15:06:58 +0300

    Add wip code to test new verification email endpoint

---
+++ pvc_cp/client.py
@@ -0,0 +1,21 @@
+from inia.client import AWSBaseClientMixin
+
+
+class EmailVerificationClient(AWSBaseClientMixin):
+    def __init__(self, access_key, secret_key, token=None, region="eu-central-1"):
+        super().__init__(access_key, secret_key, token=token, region=region)
+
+        self.service = "sso-directory"
+        self.endpoint = (
+            f"https://pvs-controlplane.{region}.prod.authn.identity.aws.dev/"
+        )
+
+        self._auth()
+
+    def verify_email(self, user_id, sso_id):
+        print("lala")
+        response = self.post(
+            "AWSPasswordControlPlaneService.StartEmailVerification",
+            {"UserId": user_id, "IdentityStoreId": sso_id},
+        )
+        return response
