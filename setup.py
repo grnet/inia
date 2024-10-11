a31787c34b9ce5b051a93fa9f354d974092cde25
Michael Loukeris <milouk@admin.grnet.gr>
2024-10-11 11:52:53 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="inia",
+    version="1.0.0",
+    description="Inia is a Python library that implements functions not included in boto3",
+    author="GRNET DevOps",
+    author_email="devops-rnd@grnet.gr",
+    license="MIT",
+    packages=["inia"],
+    zip_safe=False,
+)
