3fdc0c313f6cc7b1d9e8914e3099b50258bbd6f5
Michael Loukeris <milouk@admin.grnet.gr>
2024-11-27 17:49:39 +0200

    Add long_description to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name="inia",
     version="1.0.0",
     description="Inia is a Python library that implements functions not included in boto3",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="GRNET DevOps",
     author_email="devops-rnd@grnet.gr",
     license="MIT",
