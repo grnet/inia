367c0d67467f8b6c769311432339128846db85d7
Michael Loukeris <milouk@admin.grnet.gr>
2024-11-28 12:56:42 +0200

    Default version to 1.0.0-dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="inia",
-    version="",
+    version="1.0.0-dev",
     description="Inia extends boto3 by adding missing functions and providing convenient wrappers for existing boto3 operations.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
